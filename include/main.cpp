49cef165e599e1bfb61a262c248028cc514551aa
souvikinator <souvikinator@pop-os.localdomain>
2021-05-29 11:56:16 +0530

    fixed timelapse image->video bug

--- include/main.cpp
+++ include/main.cpp
@@ -4,38 +4,56 @@
 #include <string>
 #include <vector>
 #include <opencv2/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
 
-int main(int argc, char *argv[])
-{
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[]) {
    std::vector<std::string> image_path;
+   std::vector<cv::Mat> image_list;
+   int codec = VideoWriter::fourcc('H','2','6','4'); 
+   int h=0,w=0;
    // req atleast two args
-   if (argc != 2)
-   {
-      std::cout << color::red << "No directory provided" << std::endl;
+   if(argc!=2){
+      std::cout<<color::red<<"No directory provided"<<std::endl;
       return 1;
    }
    std::string path = argv[1];
-   std::cout << color::green << "test : 1" << color::reset << std::endl;
-   for (const auto &entry : std::experimental::filesystem::directory_iterator(path))
-   {
+
+   std::cout << color::green << "test : 1"<< color::reset << std::endl;
+   for (const auto & entry : std::experimental::filesystem::directory_iterator(path)) {
       std::string img_path = entry.path();
-      if ((img_path.find(".jpg") != std::string::npos) or (img_path.find(".jpeg") != std::string::npos) or (img_path.find(".png") != std::string::npos))
-      {
+      if((img_path.find(".jpg") != std::string::npos) or (img_path.find(".jpeg") != std::string::npos) or (img_path.find(".png") != std::string::npos)) {
          image_path.push_back(img_path);
       }
    }
 
-   if (image_path.size() == 0)
-   {
-      std::cout << color::red << "No image files in the current directory" << color::reset << std::endl;
+   if(image_path.size() == 0) {
+      std::cout << color::red << "No image files in the current directory" << color::reset <<std::endl;
       return 1;
+   } 
+   else {
+    for(auto i : image_path) {
+        cv::Mat image = cv::imread(i,cv::IMREAD_COLOR);
+        image_list.push_back(image);
+        cv::resize(image, image,image_list[0].size());
+    }
    }
-   else
-   {
-      for (auto i : image_path)
-      {
-         
-      }
+
+   Size S = image_list[0].size();
+   VideoWriter outputVideo;
+   outputVideo.open("asa.avi", codec,15.0,S,true);
+   
+   if (!outputVideo.isOpened()){
+        cout  << "Could not open the output video for write: "<< endl;
+        return -1;
    }
+   for(int i=0; i<image_list.size(); i++){
+              outputVideo << image_list[i];
+   }
+
+   cout << "Finished"<<endl;
    return 0;
 }
