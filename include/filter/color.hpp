0381fced29d20d7a78da33167e9733f62fb51f7e
Rupanshi Chawda <rc80614@gmail.com>
2021-06-02 20:10:25 +0530

    filter

---
+++ include/filter/color.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+namespace color {
+   const std::string red = "\033[31m";
+   const std::string green = "\033[32m";
+   const std::string blue = "\033[34m";
+   const std::string yellow = "\033[33m";
+   const std::string grey = "\033[30:1m";
+   const std::string reset = "\033[0m";
+}
